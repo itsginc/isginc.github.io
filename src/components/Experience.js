a0ec5c7307f6c830ee46cb3fe876143e712bada9
itsginc <52801048+itsginc@users.noreply.github.com>
2024-07-11 16:11:06 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,8 +4,6 @@ import {
   useScroll,
 } from "framer-motion";
 import LiIcon from "./LiIcon";
-
-
 const Details = ({ position, company, companyLink, time, address, work }) => {
   const ref = useRef(null);
   return (
@@ -24,7 +22,8 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
           <a
             className="capitalize text-primary dark:text-primaryDark"
             href={companyLink}
-            target={"_blank"}
+            target="_blank"
+            rel="noopener noreferrer"
           >
             @{company}
           </a>
@@ -32,12 +31,44 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
         <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
           {time} | {address}
         </span>
-        // <p className="font-medium w-full md:text-sm"> {work}</p>
-        <div dangerouslySetInnerHTML={{ __html: work }} />
+        <div className="font-medium w-full md:text-sm" dangerouslySetInnerHTML={{ __html: work }} />
       </motion.div>
     </li>
   );
 };
+
+// const Details = ({ position, company, companyLink, time, address, work }) => {
+//   const ref = useRef(null);
+//   return (
+//     <li
+//       ref={ref}
+//       className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
+//     >
+//       <LiIcon reference={ref} />
+//       <motion.div
+//         initial={{ y: 50 }}
+//         whileInView={{ y: 0 }}
+//         transition={{ duration: 0.5, type: "spring" }}
+//       >
+//         <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+//           {position}{" "}
+//           <a
+//             className="capitalize text-primary dark:text-primaryDark"
+//             href={companyLink}
+//             target={"_blank"}
+//           >
+//             @{company}
+//           </a>
+//         </h3>
+//         <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+//           {time} | {address}
+//         </span>
+//         // <p className="font-medium w-full md:text-sm"> {work}</p>
+//         <div dangerouslySetInnerHTML={{ __html: work }} />
+//       </motion.div>
+//     </li>
+//   );
+// };
 
 const Experience = () => {
 
