996fb27b23cfb01833b087218a8d5a7a539ea53a
itsginc <52801048+itsginc@users.noreply.github.com>
2024-07-12 12:58:38 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default function Home() {
                 className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
-              As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+                  Passionate about intercultural communication and technology, I leverage my expertise in digital media and data analysis to drive impactful projects. Beyond my professional pursuits, I find joy in tango, band performances, and backpacking adventures.
               </p>
               <div className="mt-2 flex items-center self-start lg:self-center">
                 <Link
