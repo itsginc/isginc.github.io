75f6c63e18ebf94a0f7e04956174c87b8fe15869
itsginc <52801048+itsginc@users.noreply.github.com>
2024-07-12 13:20:20 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "./Layout";
 
 const Footer = () => {
-  return (
+  // return (
     // <footer
     //   className="w-full border-t-2 border-solid border-dark
     // font-medium text-lg dark:text-light dark:border-light sm:text-base
@@ -32,7 +32,7 @@ const Footer = () => {
     //     </Link>
     //   </Layout>
     // </footer>
-  );
+  // );
 };
 
 export default Footer;
