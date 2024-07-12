8b2db1e40fa926b90a022c69857ac9dcbb905db5
itsginc <52801048+itsginc@users.noreply.github.com>
2024-07-12 12:26:20 -0400

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -43,18 +43,23 @@ const Skills = () => {
         Web
       </motion.div>
 
-        <Skill name="HTML" x="-20vw" y="2vw" />
+        <Skill name="Python" x="-20vw" y="2vw" />
 
-          <Skill name="CSS" x="-5vw" y="-10vw" />
-          <Skill name="JavaScript" x="20vw" y="6vw" />
-          <Skill name="ReactJS" x="0vw" y="12vw" />
-          <Skill name="NextJS" x="-20vw" y="-15vw" />
-          <Skill name="GatsbyJS" x="15vw" y="-12vw" />
-          <Skill name="Github" x="-35vw" y="-5vw" />
-          <Skill name="Web Design" x="32vw" y="-5vw" />
-          <Skill name="Figma" x="0vw" y="-20vw" />
-          <Skill name="Firebase" x="-25vw" y="18vw" />
-          <Skill name="Tawilwind CSS" x="28vw" y="18vw" />
+          <Skill name="R" x="-5vw" y="-10vw" />
+          <Skill name="SQL" x="-5vw" y="-10vw" />
+          <Skill name="Github" x="-5vw" y="-10vw" />
+          <Skill name="Lightroom" x="20vw" y="6vw" />
+          <Skill name="Adobe Premiere" x="0vw" y="12vw" />
+          <Skill name="Fluent English" x="-20vw" y="-15vw" />
+          <Skill name="Native Mandarin" x="15vw" y="-12vw" />
+          <Skill name="Growth mindset" x="-35vw" y="-5vw" />
+          <Skill name="Culture awareness" x="32vw" y="-5vw" />
+          <Skill name="Adaptability" x="0vw" y="-20vw" />
+          <Skill name="Interpersonal skills" x="-25vw" y="18vw" />
+          <Skill name="Conversational Spanish" x="28vw" y="18vw" />
+          <Skill name="Tango" x="0vw" y="-20vw" />
+          <Skill name="Band performance" x="-25vw" y="18vw" />
+          <Skill name="Backpacking" x="28vw" y="18vw" />
     </div></>
   );
 };
