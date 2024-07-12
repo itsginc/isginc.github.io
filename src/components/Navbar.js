2c86922294b2cdb682ff9dab28a9f18ddd037886
itsginc <52801048+itsginc@users.noreply.github.com>
2024-07-12 13:44:32 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -99,20 +99,20 @@ const Navbar = () => {
         className="flex items-center justify-center flex-wrap lg:mt-2
       "
       >
-        <motion.a
-          target={"_blank"}
-          className="w-6 mr-3"
-          href="#"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          aria-label="Checkout my twitter profile"
-        >
-          <TwitterIcon />
-        </motion.a>
+        // <motion.a
+        //   target={"_blank"}
+        //   className="w-6 mr-3"
+        //   href="#"
+        //   whileHover={{ y: -2 }}
+        //   whileTap={{ scale: 0.9 }}
+        //   aria-label="Checkout my twitter profile"
+        // >
+        //   <TwitterIcon />
+        // </motion.a>
         <motion.a
           target={"_blank"}
           className="w-6 mx-3"
-          href="#"
+          href="https://github.com/itsginc"
           whileHover={{ y: -2 }}
           whileTap={{ scale: 0.9 }}
           aria-label="Checkout my github profile"
@@ -122,33 +122,33 @@ const Navbar = () => {
         <motion.a
           target={"_blank"}
           className="w-6 mx-3"
-          href="#"
+          href="https://www.linkedin.com/in/ginny-cmy/"
           whileHover={{ y: -2 }}
           whileTap={{ scale: 0.9 }}
           aria-label="Checkout my linkedin profile"
         >
           <LinkedInIcon />
         </motion.a>
-        <motion.a
-          target={"_blank"}
-          className="w-6 mx-3 bg-light rounded-full"
-          href="#"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          aria-label="Checkout my pinterest profile"
-        >
-          <PinterestIcon />
-        </motion.a>
-        <motion.a
-          target={"_blank"}
-          className="w-6 mx-3"
-          href="#"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          aria-label="Checkout my dribbble profile"
-        >
-          <DribbbleIcon />
-        </motion.a>
+        // <motion.a
+        //   target={"_blank"}
+        //   className="w-6 mx-3 bg-light rounded-full"
+        //   href="#"
+        //   whileHover={{ y: -2 }}
+        //   whileTap={{ scale: 0.9 }}
+        //   aria-label="Checkout my pinterest profile"
+        // >
+        //   <PinterestIcon />
+        // </motion.a>
+        // <motion.a
+        //   target={"_blank"}
+        //   className="w-6 mx-3"
+        //   href="#"
+        //   whileHover={{ y: -2 }}
+        //   whileTap={{ scale: 0.9 }}
+        //   aria-label="Checkout my dribbble profile"
+        // >
+        //   <DribbbleIcon />
+        // </motion.a>
 
         <button
           onClick={() => setMode(mode === "light" ? "dark" : "light")}
